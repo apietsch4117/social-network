75ffd182fad09f8e31368a9d111ecff37b120898
Amanda Pietsch <amanda.ann.pietsch@gmail.com>
2023-02-03 14:06:14 -0800

    submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# social-network
+# Social-Network
+
+## Description
+
+This is a simple social media network that uses mongo database. Friends can follow/unfollow friends on their own accounts. Yarn lock is used to control the dependencies. 
+
+## Usage
+
+Not currently implemented, just completed the basic code functionality
+
+
+## Submission
+
+Github: https://github.com/apietsch4117/social-network.git
---
+++ routes/posts.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can only update your own post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can only delete your own post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
